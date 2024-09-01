6d46679991f8ac2105c0fa8fd05d0af209149295
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-09-01 12:35:48 +0500

    new session

---
+++ variables.js
@@ -0,0 +1,20 @@
+const accountId = 116641644
+let accountEmail = "fasdfas@gmail.com"
+var accountPassword = "64716344"
+
+accountCity = "Karachi"
+let accountState;
+
+// const accountId = 5464
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail = "fjghjgjuyt@gmail.com"
+accountPassword = "646446164"
+accountCity = "Islamabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])

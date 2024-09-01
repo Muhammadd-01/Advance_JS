37dcb4abc646c0e56ed0a8184281ce334cc2a3c1
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-09-01 12:56:07 +0500

    Data types

---
+++ Datatypes&Echma.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS as newer version
+
+
+console.log(3 
+    +       //code readability that should be high
+    3);
+
+
+let name = "Affan"
+let age = 19
+let isLoggedIn = true
+
+    // number => 2 to power 53
+    // bigint => 2 to power 64
+    // string => ""
+    // boolean => true or false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+
+    //object
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
+    

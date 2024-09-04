062f24240041918a27407ea32de523b8359e25a9
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-09-04 15:34:49 +0500

    Conversion of Datatypes

---
+++ ConversionOperation.js
@@ -0,0 +1,36 @@
+let score = "33abc"
+
+
+//console.log (typeof(score))
+
+
+let valueInNumber = Number(score)
+
+
+//console.log (typeof valueInNumber)
+
+
+
+//onsole.log (valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true & 0 => false
+// "" => false
+// "Affan" => true
+
+let someNumber = 32
+
+let stringNumber = String(someNumber)
+
+
+console.log(typeof stringNumber)

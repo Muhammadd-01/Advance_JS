92d707863fd118f5051a55d7035d121cd64a59c1
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-09-04 15:53:20 +0500

    Oerations

---
+++ Operations.js
@@ -0,0 +1,33 @@
+let value = 3
+let negativeValue = -value
+
+//console.log(negativeValue);
+
+let name1 = "Muhammad"
+let name2 = " Affan"
+
+let name3 = name1+name2
+
+//console.log(name3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); 
+// console.log(1 + 2 + "2"); 
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+
+gameCounter++;
+
+console.log(gameCounter);
+
+

8f8d9b4e8ec4b36f9ba20c0183e876aef0743d68
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-09-28 13:01:01 +0500

    Changes

---
+++ Number&Maths.js
@@ -0,0 +1,26 @@
+//Number in JavaScript
+
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(4414)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(1));
+// console.log(balance.toExponential(2));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+
+
+//Maths in JavaScript
+
+

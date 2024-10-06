f75ba2ccbe18675390d66fab35494459a6185172
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-10-06 10:22:55 +0500

    dates

---
+++ Dates&Time.js
@@ -0,0 +1,13 @@
+//Dates
+
+const newDate = new Date()
+
+// console.log(newDate.toString());
+// console.log(newDate.toISOString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toJSON());
+// console.log(newDate.toLocaleString());
+
+
+const myDate = new Date(2022,1,15,5,31)
+console.log(myDate);

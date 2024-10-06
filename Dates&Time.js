87448bceb2a520318dbdfa39048ba36d4e821291
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-10-06 10:40:48 +0500

    Dates & Times

--- Dates&Time.js
+++ Dates&Time.js
@@ -2,27 +2,27 @@
 
 const newDate = new Date()
 
-// console.log(newDate.toString());
-// console.log(newDate.toISOString());
-// console.log(newDate.toDateString());
-// console.log(newDate.toJSON());
-// console.log(newDate.toLocaleString());
+console.log(newDate.toString());
+console.log(newDate.toISOString());
+console.log(newDate.toDateString());
+console.log(newDate.toJSON());
+console.log(newDate.toLocaleString());
 
 
 const myDate = new Date(2022,1,15,5,31)
-// console.log(myDate);
+console.log(myDate);
 
 
 //Times
 
 
 const newTime = Date.now()
-// console.log(Math.floor(myDate.getTime()/1000));
+console.log(Math.floor(myDate.getTime()/1000));
 
 
 const myNewDate = new Date()
-// console.log(myNewDate.getTime());
-// console.log(myNewDate.getMonth());
+console.log(myNewDate.getTime());
+console.log(myNewDate.getMonth());
 
 
 

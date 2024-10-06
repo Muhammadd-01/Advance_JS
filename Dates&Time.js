2418f36aae5017ea615d45647762dd0ce362ca43
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-10-06 10:39:31 +0500

    Date & TImes

--- Dates&Time.js
+++ Dates&Time.js
@@ -10,4 +10,25 @@ const newDate = new Date()
 
 
 const myDate = new Date(2022,1,15,5,31)
-console.log(myDate);
+// console.log(myDate);
+
+
+//Times
+
+
+const newTime = Date.now()
+// console.log(Math.floor(myDate.getTime()/1000));
+
+
+const myNewDate = new Date()
+// console.log(myNewDate.getTime());
+// console.log(myNewDate.getMonth());
+
+
+
+console.log(myNewDate.toLocaleDateString('defualt',{
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+}));

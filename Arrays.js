632560048b1b58ed54cbfd808207b7826822427f
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-10-13 18:04:58 +0500

    Array

--- Arrays.js
+++ Arrays.js
@@ -17,11 +17,22 @@ const newArray = ['Affan', 'Alam'];
 
 
 
-myArray.join
 
-console.log(myArray);
 
-console.log('A', myArray);
+// const myn2 = myArray.join()
+// const myn2 = myArray.includes(3)
+// const myn2 = myArray.indexOf(5)
 
-const myn1 = myArray.slice(1,3)
-console.log(myn1);   
+// console.log(myn2);
+
+console.log('A', myArray)
+
+const myn1 = myArray
+
+
+const myn2 = myArray.slice(1,3)
+console.log('B', myn2)
+
+const myn3 = myArray.splice(1,3)
+console.log('C', myArray)
+

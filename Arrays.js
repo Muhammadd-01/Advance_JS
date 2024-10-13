3b6a0aeef391f8a5e2827f3693352ecd9ded9181
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-10-13 17:53:58 +0500

    Array

--- Arrays.js
+++ Arrays.js
@@ -1,8 +1,27 @@
 //Array 
 
-const myArray = [0,64,554,5];
+const myArray = [0,1,2,4,5];
 
 const newArray = ['Affan', 'Alam'];
-console.log(myArray[2]);
-console.log(newArray[1]);
+// console.log(myArray[2]);
+// console.log(newArray[1]);
 
+//Array Methods
+
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop();
+// myArray.unshift(48);
+// myArray.shift();
+
+
+
+myArray.join
+
+console.log(myArray);
+
+console.log('A', myArray);
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1);   

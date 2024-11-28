6465bd0f3ccf29d982ee494b67844185aa29b522
Muhammad Affan <muhammadaffan1445@gmail.com>
2024-11-28 10:47:04 +0500

    changes

---
+++ Array2.js
@@ -0,0 +1,10 @@
+const marvelHeroes = [ "thor", "ironman", "spiderman" ];
+const dcHeroes = [ "superman", "flash", "batman" ];
+
+// marvelHeroes.push(dcHeroes);
+// console.log(marvelHeroes);
+// console.log(marvelHeroes[3][1]);
+
+
+// const allHeroes = marvelHeroes.concat(dcHeroes);
+// console.log(allHeroes);

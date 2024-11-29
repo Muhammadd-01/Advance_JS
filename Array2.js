ddc603f8db38aa7e4272b567d442958a9866f700
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-11-29 13:18:02 +0000

    Array

--- Array2.js
+++ Array2.js
@@ -41,5 +41,6 @@ console.log(Array.from({name : "affan"}));
 let num1 = 10;
 let num2 = 20;
 let num3 = 30;
+let num4 = 40;
 
-console.log(Array.of(num1, num2, num3));
+console.log(Array.of(num1, num2, num3, num4));

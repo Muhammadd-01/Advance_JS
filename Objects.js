31a378a6cf1c46cac723c0f070612cca697fb65d
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-11-29 13:54:14 +0000

    Object

--- Objects.js
+++ Objects.js
@@ -3,10 +3,18 @@
 
 // Object literals
 
+const mySym = Symbol("key1");
+
 const data = {
     name: "affan",
+    "full name": "Muhammad affan",
+    [mySym]: "mykey",
     age: "19",
     location: "Karachi",
     isLoggedIn: true,
     lastLoginDays : ["Monday", "Tuesday"],
 }
+
+console.log(data.name); // One method to acceess data
+console.log(data["full name"]); // Another method to access data
+console.log(data[mySym]); // method to access symbol

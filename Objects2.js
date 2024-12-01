2034fcb751ba3c1c555789b1e39ad38f25a8dfd5
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-12-01 15:32:04 +0000

    Object

--- Objects2.js
+++ Objects2.js
@@ -33,3 +33,31 @@ const obj3 = {5: "e", 6: "f"}
 // const obj4 = {...obj1, ...obj2, ...obj3}; 	// use mostly
 
 // console.log(obj4);
+
+
+// Objects in Arrays
+
+
+const users = [
+    {id: 1, name: "Affan"},
+    {id: 2, name: "Ali"},
+    {id: 3, name: "Asad"},
+]
+
+
+console.log(users[1].name);
+
+
+// to check all keys of object // Note : keys() & Values() return array of keys
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); // return all key value pairs in array of arrays
+
+
+// to check is propert exist in object or not // Note : hasOwnProperty() return boolean value
+
+console.log(tinderUser.hasOwnProperty("name"));
+
+

dcaf7c39fbb8f63e0f3a29ce5fd5573bef144344
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-12-01 15:13:00 +0000

    Object

--- Objects2.js
+++ Objects2.js
@@ -5,5 +5,19 @@ const tinderUser = {}  // non singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Affan"
-tinderUser.age = 25``
+tinderUser.age = 25
 // console.log(tinderUser);
+
+
+//Objects in Objects
+
+const person = {
+    email : "affan@gmail",
+    userName : {
+        userFirstName : "Muhammad",
+        userLastName : "Affan"
+    }
+}
+
+console.log(person.userName.userLastName);
+``

3ac08d77a980a803bc80c8fed16bd9ee947a70b7
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-12-01 15:22:23 +0000

    Object

--- Objects2.js
+++ Objects2.js
@@ -19,5 +19,17 @@ const person = {
     }
 }
 
-console.log(person.userName.userLastName);
-``
+// console.log(person.userName.userLastName);
+
+
+//merging two or more objects
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3); // Not use mostly
+
+// const obj4 = {...obj1, ...obj2, ...obj3}; 	// use mostly
+
+// console.log(obj4);

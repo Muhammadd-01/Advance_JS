704f6f848da0d7c6091fdd713fda58d206166ada
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-12-03 05:20:24 +0000

    Object De-Structure and JSON

--- ObjectDe-Sructure&JSON.js
+++ ObjectDe-Sructure&JSON.js
@@ -15,3 +15,21 @@ const {name: firtName} = myObj // Another Method
 // console.log(name);  /
 
 // console.log(firtName);
+
+
+// Json Syntax in both objects and arrays
+
+// In Objects
+{
+    "name" : "Affan",
+    "age" : 20,
+    "city" : "Karachi"
+}
+
+// In Arrays
+
+[
+    {},
+    {},
+    {}
+]

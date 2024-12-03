f5387fe701d083c645ab57d46dcf7a7ed9d62eb0
Muhammadd-01 <muhammadaffan1445@gmail.com>
2024-12-03 05:10:57 +0000

    Object

---
+++ ObjectDe-Sructure&JSON.js
@@ -0,0 +1,17 @@
+// De-Structuring Object
+
+
+const myObj = {
+    name : "Affan",
+    age : 20,
+    city : "Karachi"
+}
+
+// console.log(myObj.age);  // Simple Method
+
+
+const {name} = myObj // Destructuring Method
+const {name: firtName} = myObj // Another Method
+// console.log(name);  /
+
+// console.log(firtName);

00f55cc10a67cf44872d933f71f608b182f1dd04
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-09 11:49:30 +0500

    Advance JS

--- Functions.js
+++ Functions.js
@@ -9,4 +9,22 @@ function name(){
     
 }
 
-name();
+//name();
+
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+    
+// }
+
+
+function add(num1, num2){
+    // let result = num1 + num2;
+    // return result;
+
+    return num1 + num2;
+}
+const result = add(10,20);
+console.log("Result:", result);
+
+

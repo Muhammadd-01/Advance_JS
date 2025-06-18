8f24377c887956fbd779f68dc00834e9d7dac9fc
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-18 11:07:51 +0500

    Advance JS

--- Functions.js
+++ Functions.js
@@ -2,127 +2,100 @@
 
 // Function is a block of code dsigned to perform a particular task multiple times.
 
-
-function name(){
-
-    console.log("Affan");
-    
+function name() {
+  console.log("Affan");
 }
 
 //name();
 
-
 // function add(num1, num2){
 //     console.log(num1 + num2);
-    
+
 // }
 
+function add(num1, num2) {
+  // let result = num1 + num2;
+  // return result;
 
-function add(num1, num2){
-    // let result = num1 + num2;
-    // return result;
-
-    return num1 + num2;
+  return num1 + num2;
 }
-const result = add(10,20);
+const result = add(10, 20);
 //console.log("Result:", result);
 
-
-function LogInUserMessage(username){
-    if(!username){
-
-        console.log("Please enter a Username");
-        
-    }
-    else{
-    return `${username} has logged in successfully.`
-
-    }
+function LogInUserMessage(username) {
+  if (!username) {
+    console.log("Please enter a Username");
+  } else {
+    return `${username} has logged in successfully.`;
+  }
 }
 
 console.log(LogInUserMessage("Affan"));
 // console.log(LogInUserMessage());
 
-
 //  Rest Operator in funcions
 
-
-function addTwoNumbers(...num1){
-    return num1;
-    
+function addTwoNumbers(...num1) {
+  return num1;
 }
-
 
 // console.log(addTwoNumbers(10, 545,4565,659659,65965));
 
-
 //Another way to use rest operator
 
-function addNumbers(num1, num2, ...num3){
-    console.log(num3);
+function addNumbers(num1, num2, ...num3) {
+  console.log(num3);
 }
 
 // addNumbers(15,61,57,4654,8564)
 
-
 //How to use Object in functions
 
+const user = {
+  name: "Affan",
+  age: 20,
+};
 
-const user={
-    name: "Affan",
-    age: 20
-}
-
-
-function userDetails(details){
-    return`Name of the user is ${details.name} and age is ${details.age}.`;
+function userDetails(details) {
+  return `Name of the user is ${details.name} and age is ${details.age}.`;
 }
 
 // console.log(userDetails(user));
 
-console.log(userDetails({
+console.log(
+  userDetails({
     name: "Unknonw",
-    age:0
-}));
-
+    age: 0,
+  })
+);
 
 // How to use Array in functions
 
-const numbers =[10, 41, 54 ,44]
+const numbers = [10, 41, 54, 44];
 
-
-function  arraySecondElement(array){
-    return array[2];
+function arraySecondElement(array) {
+  return array[2];
 }
 
 // console.log(arraySecondElement(numbers));
 
-
-console.log(arraySecondElement([
-    10, 20, 30, 40, 50
-]));
-
-
-
+console.log(arraySecondElement([10, 20, 30, 40, 50]));
 
 //This keyword and Arrow functions
 
 const userDetail = {
-    name: "Affan",
-    age: 20,
-    welcomeMessage: function(){
-        console.log(`Welcome ${this.name}`);
+  name: "Affan",
+  age: 20,
+  welcomeMessage: function () {
+    console.log(`Welcome ${this.name}`);
 
-        console.log(this);
-        
-        
-    }
-}
+    console.log(this);
+  },
+};
 
 // userDetail.welcomeMessage();
 
 // console.log(this);
-
 
 //Arrow function
 
@@ -131,9 +104,19 @@ const userDetail = {
 // }
 // const addNum= (num1, num2)=> num1 + num2;
 // const addNum= (num1, num2)=> (num1 + num2)
-const addNum = (num1, num2)=> ({ name:'Affan' })
+const addNum = (num1, num2) => ({ name: "Affan" });
 
+// console.log(addNum(10,20));
 
+const obj = {
+  val: 42,
+  method: function () {
+    const inner = () => {
+      console.log(`Value is ${this.val}`);
+    };
 
+    inner();
+  },
+};
 
-console.log(addNum(10,20));
+obj.method();

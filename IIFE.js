3e8cc6aa7f99b00046c788e986265eb1c4d10762
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-18 11:56:58 +0500

    Advance JS

--- IIFE.js
+++ IIFE.js
@@ -1,6 +1,6 @@
 //Imediately Invoked Function Expression (IIFE)
 
-// Named IIFE
+//Named IIFE
 (function DB(){
     console.log(`DB CONNECTED`);
     
@@ -16,9 +16,50 @@
 })("Affan")
 
 
+var name = "Affan";
+
+(()=>{
+    var name = "Ali"
+    console.log(`Hello ${name}`);
+    
+})()
+
+name = "Ahmed";
+
+
+ function user(){
+
+    var name = "Ali";
+    return(`Hello ${name}`)
+ }
 
 
 
+ function userTwo(){
+    name = "Affan";
+    return(`Hello ${name}`)
+ }
+console.log(user());
 
+    console.log(userTwo());
 
+    console.log(name);
+    
 
+(()=>{
+    let name = "Ali";
+    function userOne(){
+        let name = "Affan";
+        return(`Hello ${name}`)
+    }
+
+    function userTwo(){
+        let name = "Ahmed";
+        return(`Hello ${name}`)
+    }
+
+    console.log(userOne());
+    console.log(userTwo());
+    console.log(name);
+    
+})();

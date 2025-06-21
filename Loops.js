3af896e5b6bc807b1cac281b6552184e1592e2c2
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-21 19:12:14 +0500

    Advance JS

--- Loops.js
+++ Loops.js
@@ -8,11 +8,33 @@ for(i = 0; i<= 10; i++){
 
 
 for(i = 0; i<=10; i++){
-    console.log(`The value of i is ${i}`);
+   // console.log(`The value of i is ${i}`);
     for(j=0; j<=10; j++){
        // console.log(`The value is ${j} and the value of i is ${i}`);
-        console.log(i + '*' + j + '=' + i*j);
+        //console.log(i + '*' + j + '=' + i*j);
         
     }
     
 }
+// Break Statement in JavaScript
+for(i =1; i<=10; i++){
+
+    if(i == 5){
+        console.log("Skipping 5");
+        break
+    }
+    console.log(i);
+    
+}
+//Continue Statement in JavaScript
+for(i =1; i<=10; i++){
+
+    if(i == 5){
+        console.log("Skipping 5");
+       continue
+    }
+    console.log(i);
+    
+}
+
+

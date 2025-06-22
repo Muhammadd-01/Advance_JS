dc095724063dd2a35c131b4b17b3b925d0fa74e7
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-22 10:48:30 +0500

    Advance JS

--- Loops.js
+++ Loops.js
@@ -1,43 +1,50 @@
 //Loops in JavaScript
 
 
-for(i = 0; i<= 10; i++){
-    // console.log(i);
+// for(i = 0; i<= 10; i++){
+   // console.log(i);
     
-}
+// }
 
 
-for(i = 0; i<=10; i++){
+// for(i = 0; i<=10; i++){
    // console.log(`The value of i is ${i}`);
-    for(j=0; j<=10; j++){
-       // console.log(`The value is ${j} and the value of i is ${i}`);
-        //console.log(i + '*' + j + '=' + i*j);
+//     for(j=0; j<=10; j++){
+     // console.log(`The value is ${j} and the value of i is ${i}`);
+      //console.log(i + '*' + j + '=' + i*j);
         
-    }
+//     }
     
-}
+
 // Break Statement in JavaScript
-for(i =1; i<=10; i++){
+// for(i =1; i<=10; i++){
 
-    if(i == 5){
-        console.log("Skipping 5");
-        break
-    }
-    console.log(i);
+//     if(i == 5){
+      // console.log("Skipping 5");
+       //break
+//     }
+   // console.log(i);
     
-}
+// }
 //Continue Statement in JavaScript
-for(i =1; i<=10; i++){
+// for(i =1; i<=10; i++){
 
-    if(i == 5){
-        console.log("Skipping 5");
-       continue
-    }
-    console.log(i);
+//     if(i == 5){
+     // console.log("Skipping 5");
+    //  continue
+//     }
+  //  console.log(i);
     
-}
+// }
 
 
 //While loop in Javascript
 
 
+let i = 0;
+
+while(i<=10){
+    console.log(i);
+   
+    i++;
+} 

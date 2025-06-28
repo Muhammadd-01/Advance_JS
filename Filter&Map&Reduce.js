ad3609b27a44f115b50d2932a3b4a0c15e805104
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-28 11:41:30 +0500

    Advance JS

---
+++ Filter&Map&Reduce.js
@@ -0,0 +1,8 @@
+const coding = ['cpp', 'java', 'js', 'python'];
+
+
+const code =coding.forEach((item)=>{
+    console.log(item);
+    
+})
+console.log(code); // undefined, because forEach does not return anything

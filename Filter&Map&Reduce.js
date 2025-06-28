4ac45de3dade9f3c424650ff9e50ff92421c2491
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-28 11:46:13 +0500

    Advance JS

--- Filter&Map&Reduce.js
+++ Filter&Map&Reduce.js
@@ -5,4 +5,14 @@ const code =coding.forEach((item)=>{
     console.log(item);
     
 })
-console.log(code); // undefined, because forEach does not return anything
+//console.log(code); // undefined, because forEach does not return anything
+
+
+//Using Filter
+
+const mynum=[1,2,3,4,5,6,7,8,9,10];
+
+
+const newNum=mynum.filter((num)=>num<5)
+console.log(newNum); // Filter returns and dont show the undefined value
+
